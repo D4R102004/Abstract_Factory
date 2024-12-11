990d14f64cd56921637a16c226ce372181064461
D4R102004 <idania.urrutia1@gmail.com>
2024-12-11 01:33:24 -0500

    Fixed bugs in html
    We fixed the error in base.html and removed the abstract factory for now

--- Abstract_Factory/abs_fact.py
+++ Abstract_Factory/abs_fact.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-class AbstractFactory:
-    def __init__(self):
-        self._p = 3
-    @property
-    def p(self):
-        return self._p
+    
     
 @app.route('/')
 @app.route('/home')
 def home_page():
-    
+    return render_template('home.html')
+
