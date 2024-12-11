d9fd4c71dadff12383d064f11d2cf5e5d1989a78
D4R102004 <idania.urrutia1@gmail.com>
2024-12-11 01:24:19 -0500

    Added templates, base.html, and home.html

--- Abstract_Factory/abs_fact.py
+++ Abstract_Factory/abs_fact.py
@@ -1,6 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
 class AbstractFactory:
     def __init__(self):
         self._p = 3
     @property
     def p(self):
         return self._p
+    
+@app.route('/')
+@app.route('/home')
+def home_page():
+    
